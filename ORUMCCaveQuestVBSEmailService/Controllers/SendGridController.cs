3b9e01a9115fb56cc4e5395276012eaeb1e9b327
Brian <briandavis1977@gmail.com>
2016-04-15 00:20:12 -0400

    [#111577624] adding web api 2 web service for handling background email requests

---
+++ ORUMCCaveQuestVBSEmailService/Controllers/SendGridController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Web.Http;
+using SendGrid;
+
+namespace ORUMCCaveQuestVBSEmailService.Controllers
+{
+    public class SendGridController : ApiController
+    {
+        public void SendEmail(string destEmailAddress, string childName)
+        {
+            var APIKey = System.Environment.GetEnvironmentVariable("SENDGRID_APIKEY");
+            var transportWeb = new Web(APIKey);
+            var email = new SendGridMessage();
+            email.AddTo(destEmailAddress);
+            email.AddBcc("briandavis1977@gmail.com");
+            email.From = new MailAddress("donotreply@oakridgeumc.org");
+            email.Subject = "CaveQuest VBS Registration for " + childName;
+            email.Text = 
+@"We have received your registration.  We look forward to seeing you on Sunday, June 19th at 6:15pm!  
+Have a blessed day!
+ 
+- The ORUMC Cave Quest VBS Team";
+            transportWeb.DeliverAsync(email);
+        }
+    }
+}
