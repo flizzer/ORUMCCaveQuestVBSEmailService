730a3fbbed07764eab23447088a1d9f7458499f7
Brian Davis <bdavis@jswcoinc.com>
2016-04-20 22:45:52 -0400

    [finishes #111577624] adding director email and assorted refactoring

--- ORUMCCaveQuestVBSEmailService/Controllers/SendGridController.cs
+++ ORUMCCaveQuestVBSEmailService/Controllers/SendGridController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -11,21 +12,62 @@ namespace ORUMCCaveQuestVBSEmailService.Controllers
 {
     public class SendGridController : ApiController
     {
-        public void SendEmail(string destEmailAddress, string childName)
+        private string _childName;
+        private string _bccEmailAddress;
+        private string _fromEmailAddress;
+        private string _emailSubject;
+        private Web _transportWeb { get; set; }
+
+        public void SendEmail(string parentDestEmailAddress, string childName, string childUniqueId)
         {
-            var APIKey = System.Environment.GetEnvironmentVariable("SENDGRID_APIKEY");
-            var transportWeb = new Web(APIKey);
+            _childName = childName;
+            InitializeTransport();
+            GetCommonEmailSettings();
+            SendParentEmail(parentDestEmailAddress);
+            SendDirectorEmail(childUniqueId);
+        }
+
+        private void GetCommonEmailSettings()
+        {
+            _bccEmailAddress = ConfigurationManager.AppSettings["BccEmailAddress"];
+            _fromEmailAddress = ConfigurationManager.AppSettings["FromEmailAddress"];
+            _emailSubject = ConfigurationManager.AppSettings["EmailSubject"];
+        }
+
+        private void SendParentEmail(string parentDestEmailAddress)
+        {
             var email = new SendGridMessage();
-            email.AddTo(destEmailAddress);
-            email.AddBcc("briandavis1977@gmail.com");
-            email.From = new MailAddress("donotreply@oakridgeumc.org");
-            email.Subject = "CaveQuest VBS Registration for " + childName;
-            email.Text = 
-@"We have received your registration.  We look forward to seeing you on Sunday, June 19th at 6:15pm!  
+            email.AddTo(parentDestEmailAddress);
+            email.AddBcc(_bccEmailAddress);
+            email.From = new MailAddress(_fromEmailAddress);
+            email.Subject = _emailSubject + _childName;
+            email.Text =
+                @"We have received your registration.  We look forward to seeing you on Sunday, June 19th at 6:15pm!  
 Have a blessed day!
  
-- The ORUMC Cave Quest VBS Team";
-            transportWeb.DeliverAsync(email);
+The ORUMC Cave Quest VBS Team";
+            _transportWeb.DeliverAsync(email);
+        }
+
+        private void InitializeTransport()
+        {
+            var APIKey = Environment.GetEnvironmentVariable("SENDGRID_APIKEY");
+            _transportWeb =  new Web(APIKey);
+        }
+
+        private void SendDirectorEmail(string childUniqueId)
+        {
+            var email = new SendGridMessage();
+            email.AddTo(ConfigurationManager.AppSettings["DirectorEmailAddress"]);
+            email.AddTo(ConfigurationManager.AppSettings["BackupEmailAddress"]);
+            email.AddBcc(_bccEmailAddress);
+            email.From = new MailAddress(_fromEmailAddress);
+            email.Subject = _emailSubject + _childName;
+            var childUniqueURL = ConfigurationManager.AppSettings["PersistentStorageURL"] + childUniqueId;
+            var emailBody = @"A new child has been registered for this year's VBS.  Use this link if you would like to view his or her information:
+{0}";
+            email.Text = string.Format(emailBody, childUniqueURL);
+            _transportWeb.DeliverAsync(email);
         }
     }
 }
